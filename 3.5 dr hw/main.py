050e283e587b3bf15a83850acad7e3dcc587059b
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-10-28 00:51:00 +0300

    Add files via upload

---
+++ 3.5 dr hw/main.py
@@ -0,0 +1,17 @@
+# summa = lambda a,b:a+b
+# print(summa(3,7))
+
+# list1 = [2,1,3,5,6,80,9]
+# summa2 = lambda sonlar=list: min(sonlar)
+# summa3 = lambda sonlar=list: max(sonlar)
+# print(summa2(list1))
+# print(summa3(list1))
+
+# natija = lambda a,b:f"{a} {b}"
+# print(natija("hello","world"))
+
+
+
+# inp1 = input("enter text: ")
+# natija1 = lambda text=str :True if text[0]=="a" else False
+# print(natija1(text=inp1))
